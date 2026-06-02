b3071aef64bf60f2e29dc75d7655d46660a94629
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-02 19:26:27 +0530

    Add adminService to fetch admin summary data

---
+++ src/services/adminService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getAdminSummary = async (token) => {
+    const response = await axios.get(
+        `${API_URL}/admin/summary`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        }
+    )
+    return response.data
+}
