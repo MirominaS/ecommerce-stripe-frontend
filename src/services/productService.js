d5e4fa75635e5d04c4a087d876bb6a3b0e3a9199
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-05 14:56:45 +0530

    Enhance getProducts function: Add pagination, search, category, and sort parameters

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getProducts = async () => {
-  const response = await axios.get(`${API_URL}/products`);
+export const getProducts = async (
+  page = 1,
+  limit = 10,
+  search = "",
+  category = "",
+  sort = ""
+) => {
+  const response = await axios.get(`${API_URL}/products`, {
+    params: {
+      page,
+      limit,
+      search,
+      category,
+      sort,
+    },
+  });
+
   return response.data;
 };
 
@@ -10,3 +25,19 @@ export const getProductById = async (id) => {
   const response = await axios.get(`${API_URL}/products/${id}`);
   return response.data;
 };
+
+
+export const importProducts = async (products, token) => {
+  const response = await axios.post(
+    `${API_URL}/products/import`,
+    {
+      products,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  )
+  return response.data;
+}
