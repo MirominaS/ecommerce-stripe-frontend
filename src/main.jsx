3e0bfc8be4c026e76db31f22e9aed854067d2568
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-08 22:48:22 +0530

    Remove unused stripe.js import to clean up main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContex.jsx";
 import { Elements } from "@stripe/react-stripe-js";
-import { stripePromise } from "./stripe.js";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
