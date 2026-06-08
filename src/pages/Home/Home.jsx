77580b120b32a8cd727244dd85da7ed2b261a33d
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-08 13:36:57 +0530

    Fetch settings from the server and update hero section with dynamic content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,13 +6,24 @@ import { addToCart } from "../../services/cartService";
 import { FaCartArrowDown } from "react-icons/fa";
 import Navbar from "../../components/Navbar/Navbar";
 import ProductCard from "../../components/ProductCard/ProductCard";
-import { showError, showInfo, showSuccess, showWarning } from "../../utils/toast";
+import {
+  showError,
+  showInfo,
+  showSuccess,
+  showWarning,
+} from "../../utils/toast";
+import Footer from "../../components/Footer/Footer";
+import { getSetting } from "../../services/adminService";
 
 const Home = () => {
   const navigate = useNavigate();
 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [settings, setSettings] = useState({
+    heroText: "Discover Amazing Products",
+    heroSubText: "New Collection 2026",
+  });
 
   const token = localStorage.getItem("token");
 
@@ -32,16 +43,30 @@ const Home = () => {
     fetchProducts();
   }, []);
 
- const handleAddToCart = (product) => {
-  if (product.stock === 0) {
-    showWarning("Product is out of stock");
-    return;
-  }
+  useEffect(() => {
+    const fetchSettings = async () => {
+      try {
+        const data = await getSetting();
 
-  addToCart(product);
+        setSettings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  showInfo("Product added to cart");
-};
+    fetchSettings();
+  }, []);
+
+  const handleAddToCart = (product) => {
+    if (product.stock === 0) {
+      showWarning("Product is out of stock");
+      return;
+    }
+
+    addToCart(product);
+
+    showInfo("Product added to cart");
+  };
 
   const handleBuyNow = async (productId) => {
     try {
@@ -79,9 +104,9 @@ const Home = () => {
 
         <div className="hero-section">
           <div className="hero-content">
-            <span className="hero-badge">New Collection 2026</span>
+            <span className="hero-badge">{settings.heroSubText}</span>
 
-            <h1>Discover Amazing Products</h1>
+            <h1>{settings.heroText}</h1>
 
             <p>
               Shop premium quality products at affordable prices with fast
@@ -113,6 +138,7 @@ const Home = () => {
           ))}
         </div>
       </div>
+      <Footer />
     </>
   );
 };
